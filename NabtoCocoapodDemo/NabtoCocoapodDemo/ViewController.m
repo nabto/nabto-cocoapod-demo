c004c5083c613d2b4a046f8c3b079c2894ecb747
Ulrik Gammelby <ug@nabto.com>
2017-02-22 17:26:26 +0000

    fetchurl from app works

--- NabtoCocoapodDemo/NabtoCocoapodDemo/ViewController.m
+++ NabtoCocoapodDemo/NabtoCocoapodDemo/ViewController.m
@@ -1,12 +1,5 @@
-//
-//  ViewController.m
-//  NabtoCocoapodDemo
-//
-//  Created by Nabto on 2/22/17.
-//  Copyright © 2017 Nabto. All rights reserved.
-//
-
 #import "ViewController.h"
+#import "NabtoClient/NabtoClient.h"
 
 @interface ViewController ()
 
@@ -14,9 +7,27 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [[NabtoClient instance] nabtoStartup];
+    [[NabtoClient instance] nabtoOpenSessionGuest];
+    NSString* interfaceXml = @"<unabto_queries><query name='wind_speed.json' id='2'><request></request><response format='json'><parameter name='rpc_speed_m_s' type='uint32'/></response></query></unabto_queries>";
+    
+    char* errorMsg;
+    if ([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg] != NABTO_OK) {
+        // handle error
+    }
+    
+    char* json;
+    nabto_status_t status = [[NabtoClient instance] nabtoRpcInvoke:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&json];
+    if (status == NABTO_OK) {
+        NSLog(@"rpcInvoke finished with result: %s", json);
+        nabtoFree(json);
+    }
+    
+    [[NabtoClient instance] nabtoShutdown];
 }
 
 
@@ -24,6 +35,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
--- NabtoCocoapodDemo/NabtoCocoapodDemoTests/NabtoCocoapodDemoTests.m
+++ NabtoCocoapodDemo/NabtoCocoapodDemoTests/NabtoCocoapodDemoTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "NabtoClient/NabtoClient.h"
 
 @interface NabtoCocoapodDemoTests : XCTestCase
 
@@ -14,26 +15,53 @@
 
 @implementation NabtoCocoapodDemoTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+
+- (void)testObjCWrapperFromDocs {
+    if ([[NabtoClient instance] nabtoStartup] != NABTO_OK) {
+        // handle error
+    }
+    if ([[NabtoClient instance] nabtoOpenSessionGuest] != NABTO_OK) {
+        // handle error
+    }
+    
+    NSString* interfaceXml = @"<unabto_queries><query name='wind_speed.json' id='2'><request></request><response format='json'><parameter name='rpc_speed_m_s' type='uint32'/></response></query></unabto_queries>";
+    
+    char* errorMsg;
+    if ([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg] != NABTO_OK) {
+        // handle error
+    }
+    
+    char* json;
+    nabto_status_t status = [[NabtoClient instance] nabtoRpcInvoke:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&json];
+    if (status == NABTO_OK) {
+        NSLog(@"rpcInvoke from docs finished with result: %s", json);
+        nabtoFree(json);
+    }
+    
+    [[NabtoClient instance] nabtoShutdown];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testNativeSdk {
+    XCTAssertEqual(nabtoStartup(NULL), NABTO_OK);
+    nabto_handle_t session;
+    XCTAssertEqual(nabtoOpenSession(&session, "guest", "123456"), NABTO_OK);
+    char* result;
+    size_t len;
+    char* mimeType;
+    nabto_status_t status;
+    status = nabtoFetchUrl(session, "nabto://demo.nabto.net/wind_speed.json?", &result, &len, &mimeType);
+    if (status == NABTO_OK) {
+        NSData *data = [NSData dataWithBytes:result length:len];
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"fetchUrl finished with result: %@", str);
+        XCTAssertTrue([str containsString:@"speed_m_s"]);
+        nabtoFree(result);
+        nabtoFree(mimeType);
+    } else {
+        NSLog(@"fetchUrl finished with status %d", status);
+    }
+    XCTAssertEqual(status, NABTO_OK);
 }
 
 @end
